2c85c787e3d821f293c48709fa43323d6e13a2fd
Mellitrissa <juliet810@mail.ru>
2023-03-13 13:31:29 +0300

    Create Pair.java

---
+++ stepik1/src/main/java/org/example/Pair.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.Objects;
+
+
+public class Pair<T, E> {
+    private T first;
+    private E second;
+
+
+    public Pair(T first, E second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public E getSecond() {
+        return second;
+    }
+
+    public static <T, E> Pair<T, E> of(T first, E second) {
+        return new Pair<>(first, second);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+}
+
+
