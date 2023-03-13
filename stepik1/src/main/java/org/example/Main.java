1ba4f7b358f4d6ca2a1762b0f9788c0f0bcd4562
Mellitrissa <juliet810@mail.ru>
2023-03-13 13:31:24 +0300

    Update Main.java

--- stepik1/src/main/java/org/example/Main.java
+++ stepik1/src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Pair<Integer, String> pair = Pair.of(1, "hello");
+        Integer i = pair.getFirst(); // 1
+        String s = pair.getSecond(); // "hello"
+
+        Pair<Integer, String> pair2 = Pair.of(1, "hello");
+        boolean mustBeTrue = pair.equals(pair2); // true!
+        boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
     }
 }
+
