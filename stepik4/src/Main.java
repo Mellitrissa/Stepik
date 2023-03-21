d2504d746653fe9570540a5b2936af8d7ba55b64
Mellitrissa <juliet810@mail.ru>
2023-03-21 12:45:10 +0300

    stepik4

---
+++ stepik4/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+    public static <T, U> Function<T, U> ternaryOperator(
+            Predicate<? super T> condition,
+            Function<? super T, ? extends U> ifTrue,
+            Function<? super T, ? extends U> ifFalse) {
+
+        return x -> (condition.test(x))?  ifTrue.apply(x) : ifFalse.apply(x);
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
